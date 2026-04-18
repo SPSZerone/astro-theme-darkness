51be118b615c3adfa6fb37a227198f9e070b6faf
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2026-04-18 12:13:18 +0800

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kpab.github.io',
-  base: '/astro-darkness',
+  site: 'https://darkness.me.seapianosnow.com',
+  base: '/',
   trailingSlash: 'always',
   integrations: [mdx(), sitemap()],
 });
