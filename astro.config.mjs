6e0364d91837c2d58d57e8b934cfdcab0d479078
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2026-04-29 13:36:02 +0800

    License for Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 const isDev = process.env.NODE_ENV === 'development';
 
 // https://astro.build/config
@@ -9,5 +11,10 @@ export default defineConfig({
   site: 'https://darkness.me.seapianosnow.com',
   base: '/',
   trailingSlash: isDev ? 'ignore' : 'always',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
+  vite: {
+    resolve: {
+      preserveSymlinks: true,
+    },
+  },
 });
